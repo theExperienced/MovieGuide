6797af81e8796256ccfcd85ff607ab38d21640be
theExperienced <mizradan@gmail.com>
2020-01-12 13:54:05 +0200

    Updates

---
+++ precache-manifest.67ba6661c20d0ff2e12ba4d1639b0379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715d94fdfde8899c5ff45858fc5f2ed5",
+    "url": "/theExperienced/MovieGuide.git/index.html"
+  },
+  {
+    "revision": "b46d2fcddf3e3ff45c9e",
+    "url": "/theExperienced/MovieGuide.git/static/css/main.c3ad657e.chunk.css"
+  },
+  {
+    "revision": "1c9dcf3f174d6a835cc7",
+    "url": "/theExperienced/MovieGuide.git/static/js/2.8b18b1ab.chunk.js"
+  },
+  {
+    "revision": "b46d2fcddf3e3ff45c9e",
+    "url": "/theExperienced/MovieGuide.git/static/js/main.563b89e3.chunk.js"
+  },
+  {
+    "revision": "5a5a8d1858e3130113ce",
+    "url": "/theExperienced/MovieGuide.git/static/js/runtime-main.91276ce0.js"
+  }
+]);
